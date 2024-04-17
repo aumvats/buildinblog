cfdd700537d8fa33c22adbae1ae9278753a0e1de
Aum Vats <its.aumvats@gmail.com>
2024-04-18 02:04:02 +0530

    Updated index.mdx and link settings in theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>BuildinBlog</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/aumvats/buildinblog',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // footer: {
+  //   text: 'Nextra Docs Template',
+  // },
 }
 
 export default config
