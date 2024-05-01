9898f8720a9484a1be4122e11ec71a37fadad25a
Aum vats <its.aumvats@gmail.com>
2024-05-01 19:19:03 +0530

    Added side bar components, test page, theme edits

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,38 @@
 export default {
-    logo: <span>BuildinBlog</span>,
+    logo: <b><span>BuildinBlog</span></b>,
     project: {
       link: 'https://github.com/aumvats/buildinblog'
+    },
+    docsRepositoryBase: 'https://github.com/aumvats/buildinblog/tree/main/pages', 
+
+    footer: {               //footer content
+      text: (
+        <span>
+          
+          <a href="https://github.com/aumvats/buildinblog" target="new-tab">
+          BuildinBlog 
+          </a> 
+          {" "}@ {new Date().getFullYear()} .
+        </span>
+      )
+    },
+    sidebar: {
+      toggleButton: 'true'
+    },
+    toc: {
+      backToTop: 'true'
+    },
+    sidebar: {
+      titleComponent({ title, type }) {
+        if (type === 'separator') {
+          return (
+            <div style={{ background: 'blue', textAlign: 'center' }}>{title}</div>
+          )
+        }
+        if (title === 'About') {
+          return <>{title}</>
+        }
+        return <>{title}</>
+      }
     }
-    // ... other theme options
   }
